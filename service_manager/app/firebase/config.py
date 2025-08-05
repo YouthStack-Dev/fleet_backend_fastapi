dae15bcd6b12f2b449915f14d55c808f370d9bdf
dheeraj kumar p <dheerajkumarp777@gmail.com>
2025-08-06 01:02:03 +0530

    changes in firabse init

--- service_manager/app/firebase/config.py
+++ service_manager/app/firebase/config.py
@@ -9,8 +9,16 @@ from firebase_admin import credentials, initialize_app, db
 firebase_key_path = os.getenv("FIREBASE_KEY_PATH", "firebase_key.json")
 
 def init_firebase():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(firebase_key_path)
-        initialize_app(cred, {
-            'databaseURL': 'https://ets-1-ccb71-default-rtdb.firebaseio.com/'
-        })
+    print("Firebase key path:", firebase_key_path)
+    if not os.path.exists(firebase_key_path):
+        raise FileNotFoundError(f"Firebase key not found at: {firebase_key_path}")
+    try:
+        if not firebase_admin._apps:
+            cred = credentials.Certificate(firebase_key_path)
+            initialize_app(cred, {
+                'databaseURL': 'https://ets-1-ccb71-default-rtdb.firebaseio.com/'
+            })
+    except Exception as e:
+        print("Error initializing Firebase:", str(e))
+        raise
+
